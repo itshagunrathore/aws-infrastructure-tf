2dd5b1f713a84ab0c46ffb3bf190a3b2567bf00b
ak255112 <ak255112@teradata.com>
2022-06-14 11:00:57 +0530

    review comments

--- core/src/dsaclient/dsaservice/dsa_job_service.go
+++ core/src/dsaclient/dsaservice/dsa_job_service.go
@@ -79,8 +79,7 @@ func (d *dsaJobService) GetSystemName(dsaDriver dsa.DsaDriver) (string, error) {
 			}
 		}
 	}
-	fmt.Println("=======================================")
-	fmt.Println(enabledSystems)
+
 	if len(enabledSystems) > 0 {
 		return enabledSystems[0].SystemName, nil
 	}
@@ -90,8 +89,6 @@ func (d *dsaJobService) GetSystemName(dsaDriver dsa.DsaDriver) (string, error) {
 func (d *dsaJobService) GetTarGetGroup(dsaDriver dsa.DsaDriver, siteTargetType models.SiteTargetType) (string, error) {
 
 	targetGroups, err := dsaDriver.GetTargetGroup(siteTargetType)
-	fmt.Println("=======================================")
-	fmt.Println(targetGroups)
 	if err != nil {
 		return "", errors.New("error while fetching target groups")
 	}
