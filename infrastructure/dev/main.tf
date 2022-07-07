91da3f9c91f704f93cc2de20eb2c772a077d228a
sr255070 <shagun.rathore@teradata.com>
2022-07-07 16:36:20 +0530

    added env tfvars

--- infrastructure/dev/main.tf
+++
@@ -1,6 +0,0 @@
-module "devmodule" {
-  source   = "../"
-  vpc_cidr = var.dev_vpc_cidr
-  engine = var.dev_engine
-
-}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  default = ""
+}
+variable "aws_secret_key" {
+  default = ""
+}
+variable "aws_access_key" {
+  default = ""
+}
+variable "db_password" {
+  description = "RDS root user password"
+  type        = string
+  sensitive   = true
+}
+variable "db_username" {
+  description = "RDS root username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_parameter_group" {}
+variable "db_subnet_group" {
+  default = ""
+}
+variable "engine" {
+  default = ""
+}
+variable "engine_version" {
+  default = ""
+}
+variable "baas_db_name" {
+  default = ""
+}
+variable "log_connections_value" {
+  default = 1
+}
+variable "parameter_group_family" {
+  default = "postgres"
+}
+variable "allocated_db_storage" {
+  default = 5
+}
+variable "pvt_subnet_A_az" {
+  default = ""
+}
+variable "pvt_subnet_B_az" {
+  default = ""
+}
+variable "instance_class" {
+  default = ""
+}
+variable "is_db_public" {
+  default = false
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "cidr_subnet_a" {
+  default = "10.0.3.0/24"
+}
+variable "cidr_subnet_b" {
+  default = "10.0.2.0/24"
+}
